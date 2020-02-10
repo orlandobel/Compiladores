1eb251791c170bf61ff30c8b780a431ecc4d2375
memotets <guillermo.bg99@gmail.com>
2020-02-09 18:39:11 -0600

    Lambda expecion added on python to do the math easy to see

--- Compiler-python/main.py
+++ Compiler-python/main.py
@@ -1,5 +1,6 @@
 from read import Read
 
 reader = Read()
-
+print(reader.readSpecialWord("void"))
 print(reader.expNum())
+print(reader.exprecion())
--- Compiler-python/read.py
+++ Compiler-python/read.py
@@ -1,6 +1,21 @@
 class Read:
     def __init__(self):
         self.file = open("test.txt","r")
+## Operaciones booleanas para crear cualquier cadena
+        self.numbers = lambda x : (ord(x) > 46 and ord(x) < 58);
+        self.upperKy = lambda x : (ord(x) > 64 and ord(x) < 91);
+        self.lowerKy = lambda x : (ord(x) > 96 and ord(x) < 123);
+        self._barKey = lambda x : (ord(x) == 95);
+## Combinaciones necesitadas
+        self.firstLetter = lambda x : (self.upperKy(x) or self.lowerKy(x) or self._barKey(x));
+        self.anyLetter =  lambda x : (self.firstLetter(x) or self.numbers(x));
+
+
+    def readSpecialWord(self, nWord):
+        if self.file.read(len(nWord)) == nWord:
+            self.readChar()
+            return nWord
+        return "Syntaxis Error"
 
     def readChar(self):
         return self.file.read(1)
@@ -9,11 +24,11 @@ class Read:
         cadena = ""
         x = self.readChar()
 
-        if((ord(x) > 64 and ord(x) < 91) or (ord(x) > 96 and ord(x) < 123) or ord(x) == 95):
+        if(self.firstLetter(x)):
             cadena += x
             x = self.readChar()
 
-            while((ord(x) > 64 and ord(x) < 91) or (ord(x) > 96 and ord(x) < 123) or (ord(x) > 46 and ord(x) < 58) or ord(x) == 95):
+            while(self.anyLetter(x)):
                 cadena += x
                 x = self.readChar()
 
@@ -22,15 +37,18 @@ class Read:
     def expNum(self):
         cadena=""
         x = self.readChar()
-        if (ord(x) > 46 and ord(x) < 58):
-            while(ord(x) > 46 and ord(x) < 58):
+        if (x == '-'):
+            cadena += x
+            x = self.readChar()
+        if (self.numbers(x)):
+            while(self.numbers(x)):
                 cadena += x
                 x = self.readChar()
             if(x == '.'):
                 cadena += x
                 x = self.readChar()
-                if (ord(x) > 46 and ord(x) < 58):
-                    while(ord(x) > 46 and ord(x) < 58):
+                if (self.numbers(x)):
+                    while(self.numbers(x)):
                         cadena += x
                         x = self.readChar()
                 else:
